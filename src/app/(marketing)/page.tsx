8e62d01e169dd8640104281bb05a16a7479b3712
Geraldo Luiz <geraldobl58@gmail.com>
2025-05-07 14:51:23 -0300

    Update CI/CD workflow to trigger on 'develop' branch and remove pull request creation step; update welcome message version to V4.2

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <div className="font-bold">Home</div>
-      <div className="text-2xl">Welcome to the: V4.1</div>
+      <div className="text-2xl">Welcome to the: V4.2</div>
       <Header />
     </div>
   )
