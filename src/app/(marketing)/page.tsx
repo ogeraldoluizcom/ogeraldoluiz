660313b8221321762e2dec4abeeb80966f4af8e8
Geraldo Luiz <geraldobl58@gmail.com>
2025-05-07 10:58:40 -0300

    Update welcome message in Home component to reflect version 2

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <div className="font-bold">Home</div>
-      <div className="text-2xl">Welcome to the: V1</div>
+      <div className="text-2xl">Welcome to the: V2</div>
       <Header />
     </div>
   )
