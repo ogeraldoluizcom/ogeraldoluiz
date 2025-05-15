92f9f87c563e2c04f16d9cee67b452a483ac4494
Geraldo Luiz <geraldobl58@gmail.com>
2025-05-15 09:50:24 -0300

    Refactor MarketingLayout to use HeaderMobile and update Home component layout for better structure

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,15 @@
 import { Hero } from '@/components/hero'
+import { SectionExperience } from '@/components/section-experience'
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-      <Hero />
+    <div className="px-4 space-y-8">
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <SectionExperience />
+      </section>
     </div>
   )
 }
