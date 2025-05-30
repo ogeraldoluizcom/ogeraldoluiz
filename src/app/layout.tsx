6fd08994adc6d9a885b3b9e5c2b1a2e1e8fe1180
Geraldo Luiz <geraldobl58@gmail.com>
2025-05-30 10:05:24 -0300

    Add web app manifest files and icons for Geraldo Luiz website
    - Created `manifest.json` for the Geraldo Luiz website with app details and icons.
    - Added `site.webmanifest` for Ogeraldoluiz with maskable icons and display settings.
    - Included PNG images for web app icons: `web-app-manifest-192x192.png` and `web-app-manifest-512x512.png`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,32 @@ import { Montserrat } from 'next/font/google'
 
 import './globals.css'
 import { RouteChangeProvider } from '@/providers/route-change-provider'
+import Script from 'next/script'
 
 const font = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Geraldo Luiz',
-  description: 'Geraldo Luiz - Criação e Desenvolvimento de Websites e Apps'
+  description: 'Geraldo Luiz - Criação e Desenvolvimento de Websites e Apps',
+  keywords: ['web development', 'apps', 'sites', 'Next.js', 'Geraldo Luiz'],
+  authors: [{ name: 'Geraldo Luiz', url: 'https://ogeraldoluiz.com' }],
+  openGraph: {
+    title: 'Geraldo Luiz',
+    description: 'Criação e Desenvolvimento de Websites e Apps',
+    url: 'https://ogeraldoluiz.com',
+    siteName: 'Geraldo Luiz',
+    images: [
+      {
+        url: 'https://ogeraldoluiz.com/img/avatar.png',
+        width: 1200,
+        height: 630,
+        alt: 'Geraldo Luiz'
+      }
+    ],
+    locale: 'pt_BR',
+    type: 'website'
+  },
+  metadataBase: new URL('https://ogeraldoluiz.com')
 }
 
 export default function RootLayout({
@@ -18,7 +38,28 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="google-site-verification"
+          content="Qsl41poLAf14XJyN-hlRD-cV_HoW-wtEXUfTaLENsks"
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PMW9DWYSNX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PMW9DWYSNX');
+          `}
+        </Script>
+      </head>
       <body className={font.className}>
         <RouteChangeProvider>{children}</RouteChangeProvider>
       </body>
